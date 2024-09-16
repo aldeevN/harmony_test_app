3ae1c5d0508028f070dbbdf50cd84ed6551a0413
AldeevN <aldeev33@gmail.com>
2024-09-16 21:44:11 +0300

    hot fixx

--- components/login.tsx
+++ components/login.tsx
@@ -20,6 +20,7 @@ export function LoginForm() {
   const [login] = useLogin();
   const router = useRouter();
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleSubmit = async (event: any) => {
     event.preventDefault();
     const formVal = {
